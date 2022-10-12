8e3df572aae61c9b500b4126d8299e463c072c96
Potestas06 <patrick@krispel.com>
2022-10-12 13:43:25 +0200

    finishd

--- files/rumore.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter your Time:', 'Input box example')}" > %TEMP%\out.tmp
-set /p OUT=<%TEMP%\out.tmp
-
-sleep OUT
--- rumore.bat
+++ rumore.bat
@@ -1,6 +1,21 @@
 @echo off
 
-powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter your name:', 'Input box example')}" > %TEMP%\out.tmp
+powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter your Time in sec:', 'Input box example')}" > %TEMP%\out.tmp
 set /p OUT=<%TEMP%\out.tmp
-set msgBoxArgs="& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('You have entered: %OUT%', 'Hello');}"
-powershell -Command %msgBoxArgs%
+
+TIMEOUT %OUT%
+start files\volume.vbs
+
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+TIMEOUT 20
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+start https://www.youtube.com/watch?v=pk7LsBI4bg4
+echo dein Computer wird heruntergefahren in:
+TIMEOUT 10
+
+shutdown
